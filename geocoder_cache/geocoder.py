488fdeba6db2f36b13083aa8d07343649796a39a
Pavelwell7 <pavelbugaev303@gmail.com>
2026-07-07 19:06:55 +0300

    update geocoder.py

--- geocoder_cache/geocoder.py
+++ geocoder_cache/geocoder.py
@@ -6,15 +6,6 @@ from geocoder_cache.models import Place
 
 
 def fetch_coordinates(address):
-
-    try:
-        place = Place.objects.get(address=address)
-        if place.lat is None or place.lon is None:
-            return None
-        return place.lat, place.lon
-    except Place.DoesNotExist:
-        pass
-
     try:
         response = requests.get(
             'https://geocode-maps.yandex.ru/1.x/',
@@ -28,49 +19,41 @@ def fetch_coordinates(address):
         response.raise_for_status()
 
         found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
         if not found_places:
-            Place.objects.create(address=address, lat=None, lon=None)
             return None
 
-        most_relevant = found_places[0]
-        lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
-        lat, lon = float(lat), float(lon)
+        lon, lat = found_places[0]['GeoObject']['Point']['pos'].split(' ')
+        return float(lat), float(lon)
 
-        Place.objects.create(
-            address=address,
-            lat=lat,
-            lon=lon,
-            updated_at=timezone.now(),
-        )
-
-        return lat, lon
-
-    except requests.exceptions.RequestException:
+    except (requests.exceptions.RequestException, KeyError, ValueError):
         return None
 
-def get_coordinates_map(addresses):
 
+def get_coordinates_map(addresses):
     cached_places = Place.objects.filter(address__in=addresses)
     coordinates_map = {}
     cached_addresses = set()
+
     for place in cached_places:
-        if place.lat is not None and place.lon is not None:
-            coordinates_map[place.address] = (place.lat, place.lon)
-        else:
-            coordinates_map[place.address] = None
+        coordinates_map[place.address] = (place.lat, place.lon) if place.lat and place.lon else None
         cached_addresses.add(place.address)
 
     missing_addresses = set(addresses) - cached_addresses
     for address in missing_addresses:
         coords = fetch_coordinates(address)
-        coordinates_map[address] = coords
 
-        Place.objects.create(
+        lat, lon = coords if coords else (None, None)
+        place, created = Place.objects.get_or_create(
             address=address,
-            lat=coords[0] if coords else None,
-            lon=coords[1] if coords else None,
-            updated_at=timezone.now(),
+            defaults={
+                'lat': lat,
+                'lon': lon,
+                'updated_at': timezone.now(),
+            }
         )
+        if not created and place.lat and place.lon:
+            coordinates_map[address] = (place.lat, place.lon)
+        else:
+            coordinates_map[address] = coords
 
     return coordinates_map
